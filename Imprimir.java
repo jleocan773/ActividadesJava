bb912365cb2a1ab873d9d3a694beec88fe7d612c
jleocan773 <jleocan773@g.educaand.es>
2022-10-20 12:46:52 +0200

    Nuevo Archivo

---
+++ Imprimir.java
@@ -0,0 +1,12 @@
+public class Imprimir {
+    public static void main(String[]args){
+        String saludo = "Hola";
+        String nombre = "Jonathan";
+        int edad = 22;
+        int primernum = 2;
+        int segunnum = 3;
+        float res = 0.67f;
+        System.out.printf("%s %s, tienes %d años \n%d \\ %d = \" %.2f \" ",saludo, nombre, edad, primernum, segunnum, res);
+    }
+}
+    
