7cc13297b83e5d871c934fb3223428e213b26c60
jleocan773 <jleocan773@g.educaand.es>
2022-10-20 12:56:45 +0200

    lol

--- Actividades/Imprimir.java
+++ Actividades/Imprimir.java
@@ -6,7 +6,10 @@ public class Imprimir {
         int primernum = 2;
         int segunnum = 3;
         float res = 0.67f;
+        System.out.printf("%s \n", saludo);
+        System.out.printf("Hola %s \n", nombre);
+        System.out.printf("Nombre: %s \nEdad: %d \n",nombre, edad);
+        System.out.println("\"Primero \\ Segundo \\ Tercero\"");
         System.out.printf("%s %s, tienes %d años \n%d \\ %d = \" %.2f \" ",saludo, nombre, edad, primernum, segunnum, res);
     }
 }
-    
