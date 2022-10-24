d8c340884fb52814cb6a7066db0dfd670bdaa9e1
jleocan773 <jleocan773@g.educaand.es>
2022-10-24 13:04:17 +0200

    nuevo

--- Actividades/Circunferencia.java
+++ Actividades/Circunferencia.java
@@ -1,9 +1,17 @@
 public class Circunferencia {
-    // Circunferencia = 2 * PI * Radio
-    final static Double PI = 3.1416;
+    // La fórmula para calcular la Circunceferencia = 2 * PI * radio
+    // La fórmula para calcular el Area = PI * Radio^2
+    // Sin usar la clase Math final static Double PI = 3.1416;
     public static void main(String[]args){
-        Integer radio = 4;
-        Double circunferencia = 2 * PI * radio;
-        System.out.println(circunferencia);
+    // Para calcular la circunferencia
+        Double radio = 4d;
+        //Sin usar la clase Math -> Double circunferencia = 2 * PI * radio;
+        Double circunferencia = Math.PI * 2 * radio;
+        System.out.println( "La circunferencia es de " + circunferencia + " cm");
+    // Para calcular el area
+        /* Sin usar la clase Math -> Double expo = Math.pow(radio, 2);
+        Double area = PI * expo; */
+        Double area = Math.PI * Math.pow(radio, 2);
+        System.out.println("El área es de " + area + " cm");
     }
 }
