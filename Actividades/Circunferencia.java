f1df1f31cdf983d12d84e6da6148323d09997266
jleocan773 <jleocan773@g.educaand.es>
2022-10-24 12:30:19 +0200

    nuevo

---
+++ Actividades/Circunferencia.java
@@ -0,0 +1,9 @@
+public class Circunferencia {
+    // Circunferencia = 2 * PI * Radio
+    final static Double PI = 3.1416;
+    public static void main(String[]args){
+        Integer radio = 4;
+        Double circunferencia = 2 * PI * radio;
+        System.out.println(circunferencia);
+    }
+}
---
+++ pruebas.java
@@ -0,0 +1,12 @@
+public class pruebas {
+    //Campos
+    String nombre;
+    //Constante
+    final static Double PI = 3.1415;
+    public static void main(String[] args){
+        //Variables
+        //String nombre;
+        System.out.println(3 + 7 + " Ho");
+        System.out.println(PI);
+    }
+}
