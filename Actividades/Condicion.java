6ea00f67874978fdeee2669e962eb89465993520
jleocan773 <jleocan773@g.educaand.es>
2022-11-02 08:46:20 +0100

    update

--- Actividades/Condicion.java
+++ Actividades/Condicion.java
@@ -1,15 +1,16 @@
 public class Condicion {
-    int x;
     public static void main(String[]args){
-        int x =1;
-        if (x >= 0){
-            if (x == 0){
-                System.out.println("A");
-            }
-            else {
+        int x =3;
+        int y =3;
+        
+        if (x >= y){
+            if (x == y){
                 System.out.println("B");
             }
-        System.out.println("C");
+            else {
+                System.out.println("C");
+            }
+        System.out.println("A");
         }
     }
 }
--- pruebas.java
+++ pruebas.java
@@ -1,25 +1,20 @@
-public class pruebas {
-int x;
-int y;
-int z;
+public class Pruebas {
+    public static void main(String[]args){
+        int diasem = 1;
 
-    public static void main (String[]args){
-        int x = 5;
-        int y = 4;
-
-        {
-            int z = 6;
-        
-            
-            System.out.println(x);
-            System.out.println(y);  
-            {
-                System.out.println(z);
-                System.out.println(x);
-                System.out.println(y);
-            }
-        }
+        if (diasem == 1)
+            System.out.println("Lunes");
+        else if (diasem == 2)
+            System.out.println("Martes");
+        else if (diasem == 3)
+            System.out.println("Miércoles");
+        else if (diasem == 4)
+            System.out.println("Jueves");
+        else if (diasem == 5)
+            System.out.println("Viernes");
+        else if (diasem == 6 || diasem == 7)
+            System.out.println("Fin de Semana");
+        else 
+            System.out.println("Valor no válido, introduzca un número del 1 al 7.");
     }
-
-    
 }
